a6425de900f662fb87fcba89d610e95f8066c670
Yitzhak-Njenga <yitzhaknjenga@gmail.com>
2022-09-26 18:52:36 +0300

    stylesheet api

--- App.js
+++ App.js
@@ -1,17 +1,30 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import {StyleSheet, View } from 'react-native';
 
 const App = () => (
-  <View style={{ flex: 1, justifyContent: 'center'}}>
-    <Box color ='red' />
-    <Box color ='green' />
-    <Box color ='blue' />
-    </View>
+  <View style={styles.layout}>
+    <View style={styles.card}/>
+    <View style={styles.card}/>
+  </View>
 );
 
 export default App;
 
-export const Box = (props) => (
-  // Move a box `View` component here
-  <View style = {{width:100, height:100, backgroundColor:props.color}} />
-);
+export const styles = StyleSheet.create({
+layout:{
+flex: 1,
+backgroundColor: '#e5e5e5',
+justifyContent: 'center'
+},
+card:{
+width: 100,
+height: 100,
+backgroundColor: 'white',
+margin: 16,
+borderRadius: 2,
+shadowColor: 'black',
+shadowOpacity: 0.3,
+shadowRadius: 1,
+shadowOffset: { height: 1, width: 0.3 },
+  },
+});
